7674dc4f74b9605b0bec8a9328dc676019edfc11
Rafael Vieira <rafaelvieirapalmital@gmail.com>
2022-03-07 16:26:02 -0300

    Adicionando modal de redefinicao de senha

--- resources/views/layouts/components/header.blade.php
+++ resources/views/layouts/components/header.blade.php
@@ -1,6 +1,8 @@
 <header class="bg-formoney p-3 d-flex justify-content-between align-items-center">
     <div class="logo">
-        <img src="{{ asset('images/logo.png') }}" alt="Logo">
+        <a title="Página principal" href="/dashboard">
+            <img src="{{ asset('images/logo.png') }}" alt="Logo">
+        </a>
     </div>
 
     <div class="mobile-menu">
